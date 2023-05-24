9a2de96ff1566e19c5f5e8ee61f2d606e1c4cdd8
keremcan23 <keremcan_2000@hotmail.com>
2023-05-24 18:52:18 +0300

    Giriş Yap Formu Tasarımı Güncellendi

--- GirisYapForm.Designer.cs
+++ GirisYapForm.Designer.cs
@@ -28,43 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GirisYapForm));
             button1 = new Button();
-            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(123, 207);
+            button1.Location = new Point(308, 326);
             button1.Name = "button1";
-            button1.Size = new Size(231, 89);
+            button1.Size = new Size(192, 41);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Giriş Yap";
             button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // label1
             // 
-            button2.Location = new Point(408, 170);
-            button2.Name = "button2";
-            button2.Size = new Size(217, 107);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(129, 196);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 31);
+            label1.TabIndex = 1;
+            label1.Text = "Kullanıcı Adı: ";
             // 
-            // Anasayfa
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(211, 257);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 31);
+            label2.TabIndex = 2;
+            label2.Text = "Şifre: ";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(308, 202);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(192, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(308, 263);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(192, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(324, 386);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(165, 20);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Şifrenizi Mi Unuttunuz? ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(308, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(192, 131);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // GirisYapForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button1);
-            Name = "Anasayfa";
-            Text = "Anasayfa";
+            Name = "GirisYapForm";
+            Text = "Giriş Yap";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
-        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
     }
 }
