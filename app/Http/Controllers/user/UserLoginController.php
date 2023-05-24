dd6c3160bff873c66d9e063bdfa36bf48ffc985a
Kutluhan İSTEK <kutluhan3423@hotmail.com>
2023-05-24 18:30:02 +0300

    Web kullanıcı login işlemi tamamlandı

--- app/Http/Controllers/user/UserLoginController.php
+++ app/Http/Controllers/user/UserLoginController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserLoginController extends Controller
 {
     public function login(Request $request){
-        $users = User::all();
-        foreach ($users as $user){
-        if($user->TC == $request->TC && $user->password == $request->password){
+        $users = User::where('TC',$request->TC)->where('password',$request->password)->first();
+
+        if($users){
             return redirect()->route('user_homepage');
             }
+        else{
+            return redirect()->route('login');
         }
+
     }
 }
