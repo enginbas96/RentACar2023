ae8c41604521e698346b8c80103f79edb5f4a3f8
keremcan23 <keremcan_2000@hotmail.com>
2023-05-24 19:02:28 +0300

    Şifremi Unuttum Ekranı Tasarımı Güncellendi

--- SifremiUnuttum.Designer.cs
+++ SifremiUnuttum.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SifremiUnuttum));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(133, 186);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Kullanıcı Adı: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(108, 244);
+            label2.Name = "label2";
+            label2.Size = new Size(177, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Kurtarma Kodu: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(303, 190);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(222, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(303, 250);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(222, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Control;
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(316, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 123);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel1.Location = new Point(258, 316);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(310, 23);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Şifrenizi Alabilmek İçin Buraya Tıklayınız";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(303, 370);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 32);
+            button1.TabIndex = 6;
+            button1.Text = "Giriş Sayfasına Dön";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // SifremiUnuttum
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            BackgroundImageLayout = ImageLayout.None;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(button1);
+            Controls.Add(linkLabel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "SifremiUnuttum";
+            Text = "Şifremi Unuttum";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private LinkLabel linkLabel1;
+        private Button button1;
     }
 }
--- SifremiUnuttum.cs
+++ SifremiUnuttum.cs
@@ -16,5 +16,10 @@ namespace RentACar2023
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
     }
 }
