236a303d1604e60ef6efe029efdc921311f4585d
keremcan23 <keremcan_2000@hotmail.com>
2023-06-13 16:23:00 +0300

    Giriş yap tasarımı güncellendi

--- GirisYapForm.Designer.cs
+++ GirisYapForm.Designer.cs
@@ -31,13 +31,13 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GirisYapForm));
             GirisYapButton = new Button();
+            ımageList1 = new ImageList(components);
             KullaniciAdiLabel = new Label();
             SifreLabel = new Label();
             GirisYapKullaniciAdiTextBox = new TextBox();
             GirisYapSifreTextBox = new TextBox();
             SifreUnuttumLinkLabel = new LinkLabel();
             KullaniciPictureBox = new PictureBox();
-            ımageList1 = new ImageList(components);
             ((System.ComponentModel.ISupportInitialize)KullaniciPictureBox).BeginInit();
             SuspendLayout();
             // 
@@ -46,7 +46,7 @@
             GirisYapButton.ImageAlign = ContentAlignment.MiddleLeft;
             GirisYapButton.ImageKey = "cikisyap.png";
             GirisYapButton.ImageList = ımageList1;
-            GirisYapButton.Location = new Point(343, 315);
+            GirisYapButton.Location = new Point(341, 342);
             GirisYapButton.Name = "GirisYapButton";
             GirisYapButton.Size = new Size(124, 41);
             GirisYapButton.TabIndex = 0;
@@ -54,11 +54,18 @@
             GirisYapButton.TextAlign = ContentAlignment.MiddleRight;
             GirisYapButton.UseVisualStyleBackColor = true;
             // 
+            // ımageList1
+            // 
+            ımageList1.ColorDepth = ColorDepth.Depth32Bit;
+            ımageList1.ImageStream = (ImageListStreamer)resources.GetObject("ımageList1.ImageStream");
+            ımageList1.TransparentColor = Color.Transparent;
+            ımageList1.Images.SetKeyName(0, "cikisyap.png");
+            // 
             // KullaniciAdiLabel
             // 
             KullaniciAdiLabel.AutoSize = true;
             KullaniciAdiLabel.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            KullaniciAdiLabel.Location = new Point(129, 196);
+            KullaniciAdiLabel.Location = new Point(129, 228);
             KullaniciAdiLabel.Name = "KullaniciAdiLabel";
             KullaniciAdiLabel.Size = new Size(152, 31);
             KullaniciAdiLabel.TabIndex = 1;
@@ -68,7 +75,7 @@
             // 
             SifreLabel.AutoSize = true;
             SifreLabel.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            SifreLabel.Location = new Point(211, 257);
+            SifreLabel.Location = new Point(211, 283);
             SifreLabel.Name = "SifreLabel";
             SifreLabel.Size = new Size(70, 31);
             SifreLabel.TabIndex = 2;
@@ -77,14 +84,14 @@
             // 
             // GirisYapKullaniciAdiTextBox
             // 
-            GirisYapKullaniciAdiTextBox.Location = new Point(308, 202);
+            GirisYapKullaniciAdiTextBox.Location = new Point(308, 228);
             GirisYapKullaniciAdiTextBox.Name = "GirisYapKullaniciAdiTextBox";
             GirisYapKullaniciAdiTextBox.Size = new Size(192, 27);
             GirisYapKullaniciAdiTextBox.TabIndex = 3;
             // 
             // GirisYapSifreTextBox
             // 
-            GirisYapSifreTextBox.Location = new Point(308, 263);
+            GirisYapSifreTextBox.Location = new Point(308, 289);
             GirisYapSifreTextBox.Name = "GirisYapSifreTextBox";
             GirisYapSifreTextBox.Size = new Size(192, 27);
             GirisYapSifreTextBox.TabIndex = 4;
@@ -92,7 +99,7 @@
             // SifreUnuttumLinkLabel
             // 
             SifreUnuttumLinkLabel.AutoSize = true;
-            SifreUnuttumLinkLabel.Location = new Point(324, 386);
+            SifreUnuttumLinkLabel.Location = new Point(324, 405);
             SifreUnuttumLinkLabel.Name = "SifreUnuttumLinkLabel";
             SifreUnuttumLinkLabel.Size = new Size(165, 20);
             SifreUnuttumLinkLabel.TabIndex = 5;
@@ -104,22 +111,17 @@
             KullaniciPictureBox.Image = (Image)resources.GetObject("KullaniciPictureBox.Image");
             KullaniciPictureBox.Location = new Point(308, 36);
             KullaniciPictureBox.Name = "KullaniciPictureBox";
-            KullaniciPictureBox.Size = new Size(192, 131);
+            KullaniciPictureBox.Size = new Size(181, 110);
             KullaniciPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             KullaniciPictureBox.TabIndex = 6;
             KullaniciPictureBox.TabStop = false;
-            // 
-            // ımageList1
-            // 
-            ımageList1.ColorDepth = ColorDepth.Depth32Bit;
-            ımageList1.ImageStream = (ImageListStreamer)resources.GetObject("ımageList1.ImageStream");
-            ımageList1.TransparentColor = Color.Transparent;
-            ımageList1.Images.SetKeyName(0, "cikisyap.png");
             // 
             // GirisYapForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
             Controls.Add(KullaniciPictureBox);
             Controls.Add(SifreUnuttumLinkLabel);
