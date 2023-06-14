d73023239a3c4c9a89030d197dd40a03a3fc6275
keremcan23 <keremcan_2000@hotmail.com>
2023-06-14 15:15:44 +0300

    .

---
+++ Form1.Designer.cs
@@ -0,0 +1,14 @@
+﻿string connetionString = null;
+MySqlConnection cnn;
+connetionString = "server=localhost;database=school;uid=root;pwd=\"\";";
+cnn = new MySqlConnection(connetionString);
+try
+{
+    cnn.Open();
+    MessageBox.Show("Connection Open ! ");
+    cnn.Close();
+}
+catch (Exception ex)
+{
+    MessageBox.Show("Can not open connection ! ");
+}
